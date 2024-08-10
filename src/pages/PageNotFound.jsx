fb4aa98177b12eac46f64d5938083782975dd5cb
AbdelrahmanEhab <abdoehab5008@gmail.com>
2024-08-10 23:01:12 +0300

    first commit

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,9 @@
+function PageNotFound() {
+    return (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+            <h1>Page Not Found ☹️😢</h1>
+        </div>
+    )
+}
+
+export default PageNotFound
